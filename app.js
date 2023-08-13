f66f993090bf3a364145261cf33489f1fd67baf1
Alexandre Marcos p. silva <103142852+alexandremarcospeixotodasilva@users.noreply.github.com>
2023-08-12 23:44:57 -0400

    atualizando rotas

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`o express esta rodando na porta ${PORT}`);
 });
 
+
+app.use(bodyParser.urlencoded({ extended: false}));
 //db connection
 db
     .authenticate()
@@ -19,4 +22,7 @@ db
 //routes
 app.get('/', (req, res) => {
     res.send("Esta funcionando");
+
 })
+
+app.use('/jobs', require('./routes/jobs'));
